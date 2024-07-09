8db6363b3f0809c6dcd6f1ae87121388e928ab83
Luis Eduardo Andrade <79378948+LuiznANdrade@users.noreply.github.com>
2024-07-09 12:21:51 -0300

    upload

---
+++ jquery/config.js
@@ -0,0 +1,7 @@
+export const config = {
+    // QUAL E A SUA UNIDADE EX.: UNOPAR; PITAGORAS; ANHANGUERA; UNIME ETC
+    unidade: "ANHANGUERA",
+    // QUAL CIDADE ESTÁ LOCALIZADO SUA UNIDADE EX.: Teixeira de Freitas - BA, PARA APARECER NO RODAPÉ DO SISTEMA
+    cidade: "Teixeira de Freitas - BA",
+    urlApiGoogle: "https://script.google.com/macros/s/AKfycbx1RDlyhWDA_7eGovAdXLbOaJFnLwhazhZQwm6GawGTCGUcGWmTfq7YOM43ZbkX5Bd6Eg/exec"
+};
